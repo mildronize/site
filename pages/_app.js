1128ad664973c00a4c223c2bb7e8aa3e2f3661fd
Thada Wangthammang <mildronize@gmail.com>
2019-11-18 23:43:57 +0700

    remove  progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import App from 'next/app'
-import NProgress from 'nprogress'
+// import NProgress from 'nprogress'
 import Router from 'next/router'
 
-NProgress.configure({ showSpinner: false });
-Router.events.on('routeChangeStart', url => {
-//   console.log(`Loading: ${url}`)
-  NProgress.start()
-})
-Router.events.on('routeChangeComplete', () => NProgress.done())
-Router.events.on('routeChangeError', () => NProgress.done())
+// NProgress.configure({ showSpinner: false });
+// Router.events.on('routeChangeStart', url => {
+// //   console.log(`Loading: ${url}`)
+//   NProgress.start()
+// })
+// Router.events.on('routeChangeComplete', () => NProgress.done())
+// Router.events.on('routeChangeError', () => NProgress.done())
 
 export default class MyApp extends App {
   render() {
