48cf982a1bcd6ba692c428e0cb4e02c99f43801a
Thada Wangthammang <mildronize@gmail.com>
2019-11-19 12:54:51 +0700

    update status page

--- pages/status.js
+++ pages/status.js
@@ -9,20 +9,20 @@ class AdminPage extends Component {
       <h1>Status</h1>
 
       <div className="row mt-5">
-        <div className="col-2">
+        <div className="col-sm-3">
           Main Web:
           </div>
-        <div className="col">
+        <div className="col-sm">
           <a href="https://app.netlify.com/sites/mildronize/deploys">
             <img src="https://api.netlify.com/api/v1/badges/ef5a7e89-ac18-4f1d-a15e-8f704e88f2ce/deploy-status" alt="Netlify Status" />
           </a>
         </div>
       </div>
-      <div className="row mt-2">
-        <div className="col-2">
+      <div className="row mt-3">
+        <div className="col-sm-3">
           Data API:
           </div>
-        <div className="col">
+        <div className="col-sm">
           <a href="https://app.netlify.com/sites/mildronize-data/deploys">
             <img src="https://api.netlify.com/api/v1/badges/99ea2040-2728-4b09-860b-f3b3f49edd72/deploy-status" alt="Netlify Status" />
           </a>
