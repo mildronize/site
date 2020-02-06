c8792d178709d9a719a0135cd7ef28f58dc8c918
Thada Wangthammang <mildronize@gmail.com>
2020-02-07 00:13:28 +0700

    fix postlist hover size

--- components/PostList.js
+++ components/PostList.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import Link from 'next/link';
-import { parseISO, format } from 'date-fns';
+import React from "react";
+import Link from "next/link";
+import { parseISO, format } from "date-fns";
 
-export default function PostList({ posts }){
-    return (
-      <div className="postlist">
+export default function PostList({ posts }) {
+  return (
+    <div className="postlist">
       {posts.map(post => (
-        <div className="row postlist-item" key={post.slug}>
-          <div className="col-sm postlist-first-column">
-            <time className="postlist-date">
-              { format(parseISO(post.date), "yyyy MMM, d") }
-              </time>
-          </div>
-          <div className="col-sm"> 
-            <Link  href='/b/[slug]' as={`/b/${post.slug}`}>
-              <a className="postlist-link"><span  dangerouslySetInnerHTML={{ __html:  post.title }} /></a>
-              </Link>
-          </div>
-        </div>
-        ))}
-    
+        <Link href="/b/[slug]" as={`/b/${post.slug}`}>
+          <a className="postlist-item-link">
+            <div className="row postlist-item" key={post.slug}>
+              <div className="col-sm postlist-first-column">
+                <time className="postlist-date">
+                  {format(parseISO(post.date), "yyyy MMM, d")}
+                </time>
+              </div>
+              <div className="col-sm">
+                <div className="postlist-link">
+                  <span dangerouslySetInnerHTML={{ __html: post.title }} />
+                </div>
+              </div>
+            </div>
+          </a>
+        </Link>
+      ))}
     </div>
-    )
+  );
 }
